ad718e87a0daf3147f361f10ee6e93232ef2454e
DaniRC <Edanielrivero@hotmail.com>
2026-04-21 12:23:43 -0300

    optimize image

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import viteImagemin from 'vite-plugin-imagemin'
+import compression from 'vite-plugin-compression'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    viteImagemin({
-      gifsicle: { optimizationLevel: 7 },
-      mozjpeg: { quality: 72 },
-      pngquant: { quality: [0.65, 0.8], speed: 4 },
-      svgo: {
-        plugins: [{ name: 'removeViewBox' }, { name: 'removeEmptyAttrs', active: false }],
-      },
-    }),
+    compression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    })
   ],
 })
